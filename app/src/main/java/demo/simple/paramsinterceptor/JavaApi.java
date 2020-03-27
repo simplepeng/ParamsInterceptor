3282509ea70b896a28e869f1e152ada5300204b2
陈鹏 <simple19930611@gmail.com>
2020-03-27 18:37:06 +0800

    update

--- app/src/main/java/demo/simple/paramsinterceptor/JavaApi.java
+++ app/src/main/java/demo/simple/paramsinterceptor/JavaApi.java
@@ -7,8 +7,9 @@ import com.readystatesoftware.chuck.ChuckInterceptor;
 import java.util.List;
 import java.util.Map;
 
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 import me.simple.interceptor.ParamsInterceptor;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -20,7 +21,11 @@ public class JavaApi {
             Map<String, String> params,
             List<String> excludeUrls) {
         OkHttpClient httpClient = new OkHttpClient.Builder()
-//                .addInterceptor(new ParamsInterceptor(params,excludeUrls,true,))
+                .addInterceptor(new ParamsInterceptor(params, excludeUrls, false, map -> {
+                    map.put("params3", "ta ta ta");
+                    map.put("params4", "tt tt tt");
+                    return null;
+                }))
                 .addInterceptor(new ChuckInterceptor(context))
                 .build();
         return new Retrofit.Builder()
